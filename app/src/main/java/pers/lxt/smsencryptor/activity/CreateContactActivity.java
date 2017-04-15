bab6afb612d42a3943bbaddf1d2aa7b25d8fe4f5
Grisw <514335188@qq.com>
2017-04-15 10:45:15 +0800

    完成添加联系人

--- app/src/main/java/pers/lxt/smsencryptor/activity/CreateContactActivity.java
+++ app/src/main/java/pers/lxt/smsencryptor/activity/CreateContactActivity.java
@@ -1,13 +1,18 @@
 package pers.lxt.smsencryptor.activity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import pers.lxt.smsencryptor.R;
+import pers.lxt.smsencryptor.database.Database;
 
 public class CreateContactActivity extends AppCompatActivity {
 
@@ -36,7 +41,28 @@ public class CreateContactActivity extends AppCompatActivity {
                 String name = nameEdt.getText().toString();
                 String publicKey = publicKeyEdt.getText().toString();
 
-                //TODO 从contacts表中添加条目
+                if(address.length() == 0){
+                    Toast.makeText(CreateContactActivity.this,"地址不能为空",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(name.length()==0){
+                    name = null;
+                }
+                if(publicKey.length()==0){
+                    publicKey = null;
+                }
+
+                SQLiteDatabase db = Database.getInstance(CreateContactActivity.this).getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("address",address);
+                values.put("name",name);
+                values.put("public_key",publicKey);
+                db.insert("contacts",null,values);
+                db.close();
+                Intent intent = new Intent(CreateContactActivity.this,MessageActivity.class);
+                intent.putExtra("phone_num",address);
+                startActivity(intent);
+                finish();
             }
         });
     }
